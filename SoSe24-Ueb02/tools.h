7854acd12585bb15318e48d5a185c41103855c97
AJsthename <italshops7@gmail.com>
2024-07-02 00:37:36 +0200

    Add files via upload

---
+++ SoSe24-Ueb02/tools.h
@@ -0,0 +1,12 @@
+#ifndef TOOLS_H_INCLUDED
+#define TOOLS_H_INCLUDED
+
+void clearBuffer();
+void clearScreen();
+void waitForEnter();
+int askYesOrNo(char *Question);
+void enter(int howManyTimes);
+void printLine(char look, int howManyTimes);
+
+
+#endif // TOOLS_H_INCLUDED
